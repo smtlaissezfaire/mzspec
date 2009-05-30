3d84571f3e5fd99ccf2197b88840887424d44834
Scott Taylor <scott@railsnewbie.com>
2009-05-30 00:58:17 -0400

    Move example-counting into a module

---
+++ src/example-counting.scm
@@ -0,0 +1,21 @@
+(module example-counting scheme
+  (define passed-example-counts
+    (lambda (lst)
+      (sum-example-counts lst #t)))
+
+  (define failed-example-counts
+    (lambda (lst)
+      (sum-example-counts lst #f)))
+
+  (define sum-example-counts
+    (lambda (lst match-value)
+      (cond
+       ((empty? lst) 0)
+       ((eq? (car lst) match-value)
+        (+ 1 (sum-example-counts (cdr lst) match-value)))
+       (else
+        (sum-example-counts (cdr lst) match-value)))))
+
+  (provide passed-example-counts
+           failed-example-counts)
+)
--- src/mzspec.scm
+++ src/mzspec.scm
@@ -1,23 +1,7 @@
 (module mzspec scheme
   (require "pluralization.scm")
+  (require "example-counting.scm")
   (require "dsl.scm")
-
-  (define passed-example-counts
-    (lambda (lst)
-      (sum-example-counts lst #t)))
-
-  (define failed-example-counts
-    (lambda (lst)
-      (sum-example-counts lst #f)))
-
-  (define sum-example-counts
-    (lambda (lst match-value)
-      (cond
-       ((empty? lst) 0)
-       ((eq? (car lst) match-value)
-        (+ 1 (sum-example-counts (cdr lst) match-value)))
-       (else
-        (sum-example-counts (cdr lst) match-value)))))
 
   (define print-examples
     (lambda (results print-fun)
